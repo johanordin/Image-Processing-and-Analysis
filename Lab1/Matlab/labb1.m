e82da8ede877869d8c9cfc5e8f8ff5ef5db306c4
chren574 <chren574@student.liu.se>
2014-11-22 19:02:08 +0100

    Komenterat och strukturerat kod

--- Lab1/Matlab/labb1.m
+++ Lab1/Matlab/labb1.m
@@ -1,26 +1,19 @@
-%% Labb 1 - 
-% - TNM087
-%%%%%%%
-%%%%%%%
-%
-clear all;
+%% Labb 1 - TNM087
+
 
 
 %% Dot 1
 % Read in and plot the gfun
 
+clear all;
 makegfun;
 load('gfun.mat');
 plot(gfun);
 hold on
 plot(2.^gfun)
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Dot 2
-%
-
-%Kontroll över index
-%[x,y] = ind2sub(size(bild1), index_M)
 
 ImgCell = {'Img1.tiff', 'Img2.tiff','Img3.tiff','Img4.tiff','Img5.tiff','Img6.tiff','Img7.tiff','Img8.tiff','Img9.tiff','Img10.tiff','Img11.tiff','Img12.tiff','Img13.tiff','Img14.tiff'};
 
@@ -31,42 +24,45 @@ ImgCell = {'Img1.tiff', 'Img2.tiff','Img3.tiff','Img4.tiff','Img5.tiff','Img6.ti
 load('gfun.mat');
 % Ladda in bilderna i en cell array
 imagesCell = {imread('Img1.tiff'), imread('Img2.tiff'),imread('Img3.tiff'),imread('Img4.tiff'),imread('Img5.tiff'),imread('Img6.tiff'),imread('Img7.tiff'),imread('Img8.tiff'),imread('Img9.tiff'),imread('Img10.tiff'),imread('Img11.tiff'),imread('Img12.tiff'),imread('Img13.tiff'),imread('Img14.tiff')};
-% Skapa en ny matris för den första bilden
-%pics4dimarray  = imread('Img1.tiff');
 
-% Skapa den 4-dimensionella matrisen med alla bilderna
-for i =1:14
+% Skapa en ny matris för den första bilden för att alokera minne
+ pics4dimarray  = imread('Img1.tiff');
+
+% Skapa den 4-dimensionella matrisen med resterande bilderna
+for i = 2:14
     pics4dimarray(:,:,:,i) = imagesCell{i};
 end
 
 % Creating a copy of the 4dim array index with zeros
 pics4dimarrayNew = pics4dimarray.*0;
-weightfunc = pics4dimarray;
+weightfunc = pics4dimarray.*0;
 
 montage(pics4dimarray);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% "Preperations assigments"
+
 % maxvärde av en av de första bilderna
 image_dark=imread('Img1.tiff');
 M = max(max(image_dark(:,:,1)));
-index_M = find(image_dark==M);
+index_M = find(image_dark == M);
 
 % minvärde av en av de sista bilderna
 image_light=imread('Img14.tiff');
 m = min(min(image_light(:,:,1)));
-index_m = find(image_light==m,1);
+index_m = find(image_light == m,1);
 
 % Medianvärde
 image_median=imread('Img9.tiff');
 med = median(median(image_median(:,:,1)));
-index_med = find(image_median==med,1);
+index_med = find(image_median == med,1);
 
 % Allokerar minne
 arr1 = zeros(14,1);
 arr2 = zeros(14,1);
 arr3 = zeros(14,1);
 
-for i=1:length(ImgCell)
+for i = 1:length(ImgCell)
     
     %temp = ImgCell{i};
     %bild = imread(temp);
@@ -82,87 +78,47 @@ plot(arr1);
 plot(arr2);
 plot(arr3);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% %% Dot 3
-% % % % intensity in picture --> 0 - 255
-% % % % gfun values maps --> 1 - 256 dvs 1 -> 0 och 256->255
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Original images to exposure images
+
+% Calutale f^-(Z) = E*delta(t)
+%for i=1:3
+    for values = 0:255
+        index = find(pics4dimarray(:,:,:,:) == values);
+        pics4dimarrayNew(index) = (2.^gfun(values+1,1)*255) / (max((2.^gfun(:,1))));
+    end
+%end
 
 time = 1/128;
 
-for i=1:14
-    for values= 0:255
-        index = find(pics4dimarray(:,:,:,i) == values);
-        
-        %pics4dimarrayNew(index) = uint8(255 * (gfun(values+1) - log2(time) )/ (abs( max(gfun(:,1)) - min(gfun(:,1)) ) ) );
-        pics4dimarrayNew(index) = uint8(255 * (gfun(values+1))/ (abs(max(gfun(:,1))) + abs(min(gfun(:,1))) )  / time);
-        time = time*2;
-    end
-end
-
-
-
-%%
-
-%% %% Dot 3
-for i=1:3
-    for values= 0:255
-        index = find(pics4dimarray(:,:,i,:) == values);
-        pics4dimarrayNew(index) = ( 2.^gfun(values+1,i)*255 ) / ( max((2.^gfun(:,i))) );
-    end
-end
-
-time = 1/128;
-
+% Divade it with exposure time to get exposure value
 for i=1:14
     pics4dimarrayNew(:,:,:,i) = pics4dimarrayNew(:,:,:,i) / time;
     time = time*2;
 end
-    
-
-%%%Plot the exposure images
-% for i=1:14
-%     figure;
-%     imshow(pics4dimarrayNew(:,:,:,i))
-% end
 
 montage(pics4dimarrayNew);
 
-%%
-%imagesCell{i}(j)+1
-
-for i = 1:14
-    for j = 1:
-        
-        z = imagesCell{i}(j)+1;
-        
-        if z <= 349697
-            newimagesCell{i}(j) = z - imagesCell{i}(1)+1;
-        else
-            newimagesCell{i}(j) = imagesCell{i}(end)+1 - z ;
-        end
-    end
-       time = time*2;
-end
-
-%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Weightfunction
 
 for i = 1:14
     
     valueMatrix = pics4dimarray(:,:,:,i).*0;
     
+    % Calculate the max/min value of all the images
     minimum = min(min(min(pics4dimarray(:,:,:,i))));
-    
     maximum = max(max(max(pics4dimarray(:,:,:,i))));
     
-    condition = ( minimum + maximum )/2;
+    condition = (minimum + maximum)/2;
     
     d = uint8(pics4dimarray(:,:,:,i) <= condition);
         
-    w1 = d.* ( pics4dimarray(:,:,:,i) - (valueMatrix+minimum) );
+    w1 = d.* (pics4dimarray(:,:,:,i) - (valueMatrix+minimum));
 
     e = uint8(pics4dimarray(:,:,:,i) > condition);
         
-    w2 = e.* ( (valueMatrix+maximum) - pics4dimarray(:,:,:,i ) );
+    w2 = e.* ((valueMatrix+maximum) - pics4dimarray(:,:,:,i ));
     
     weightfunc(:,:,:,i) = w1+w2;
     
@@ -170,64 +126,52 @@ end
 
 montage(weightfunc)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 
 
-
-%%
-
-A  = ( (weightfunc.*pics4dimarrayNew)./weightfunc);
+A  = ((weightfunc.*pics4dimarrayNew)./weightfunc);
 
 montage(A);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% "Exposure time - testing"
 
-%%
 % Set some time constant
-time = 1/128;
-
-time2 = 1;
-
-storage = zeros(14,1);
-storage2 = zeros(14,1);
-for x=1:14
-
-    storage(x) = log2(time);
-    
-    storage2(x) = log2(time2);
-    
-    time = time*2;
-    time2 = time2*2;
-end
-
-
-plot(storage);
-hold on ;
-plot(storage2);
-%%
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % Skapa en 3x3 matris
-A = [1, 2, 1;2 4 5; 3 5 6]
-% skapa en 3x3x2 matris (3-dim)
-A(:,:,2) = [1 2 2;7 6 5; 2 5 2]
-% hitta värdet 7
-index = find(A(:,:,:) == 7)
-A(index) = 16
-
-index = find(A(:) == 1)
-A(:,:,3) = [1 2 2;7 6 5; 2 5 2]
-
-%Skapa en 3x3x3x2 matris (4-dim)
-A(:,:,:,2) = A
-index = find(A(:) == 1)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% time = 1/128;
+% time2 = 1;
+% 
+% storage = zeros(14,1);
+% storage2 = zeros(14,1);
+% for x=1:14
+% 
+%     storage(x) = log2(time);
+%     storage2(x) = log2(time2);
+%     
+%     time = time*2;
+%     time2 = time2*2;
+% end
+% 
+% plot(storage);
+% hold on ;
+% plot(storage2);
 
 
+%% "Learn about matrix"
 
-% % % for i=1:3
-% % %     for values= 0:255
-% % %         index = find(pics4dimarray(:,:,i,:) == values);
-% % %         pics4dimarrayNew(index) = ( 2.^gfun(values+1,i)*255 ) / ( max((2.^gfun(:,i))) );
-% % %     end
-% % % end
+% % % Skapa en 3x3 matris
+% A = [1, 2, 1;2 4 5; 3 5 6]
+% % skapa en 3x3x2 matris (3-dim)
+% A(:,:,2) = [1 2 2;7 6 5; 2 5 2]
+% % hitta värdet 7
+% index = find(A(:,:,:) == 7)
+% A(index) = 16
+% 
+% index = find(A(:) == 1)
+% A(:,:,3) = [1 2 2;7 6 5; 2 5 2]
+% 
+% %Skapa en 3x3x3x2 matris (4-dim)
+% A(:,:,:,2) = A
+% index = find(A(:) == 1)
 
 
 %% Sample of small 4-dim array
@@ -237,6 +181,20 @@ index = find(A(:) == 1)
 %     index = find(A(:) == values);
 %     A(index) = gfun(values+1);
 % end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
 
+
+%% 
+% % % % intensity in picture --> 0 - 255
+% % % % gfun values maps --> 1 - 256 dvs 1 -> 0 och 256->255
+
+% time = 1/128;
+% 
+% for i=1:14
+%     for values= 0:255
+%         index = find(pics4dimarray(:,:,:,i) == values);
+%         
+%         pics4dimarrayNew(index) = uint8(255 * (gfun(values+1))/ (abs(max(gfun(:,1))) + abs(min(gfun(:,1))) )  / time);
+%         
+%         time = time*2;
+%     end
+% end
